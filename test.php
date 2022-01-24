abf8339b24aca3fd7641cb7fbcf94a3f3f77a860
carmel26 <nkeshipelagie@gmail.com>
2022-01-24 22:36:03 +0300

    correction et affichage du resultat du 24-1-2022 a 22h35

--- test.php
+++ test.php
@@ -1,10 +1,10 @@
 <?php 
-  $cour[] = array('cour'=>'francais','vh'=>4) ;
-  $cour[] = array('cour'=>'anglais','vh'=>3);
+  $cour[] = array('cour'=>'francais','vh'=>6) ;
+  $cour[] = array('cour'=>'anglais','vh'=>5);
   $cour[] = array('cour'=>'kirundi','vh'=>5);
   $cour[] = array('cour'=>'maths','vh'=>3);
   $cour[] = array('cour'=>'technologie','vh'=>6);
-  $cour[] = array('cour'=>'biologie','vh'=>4);
+  $cour[] = array('cour'=>'biologie','vh'=>8);
   $cour[] = array('cour'=>'civisime','vh'=>2);
   $cour[] = array('cour'=>'em','vh'=>5);
   $cour[] = array('cour'=>'geographie','vh'=>4);
@@ -17,52 +17,103 @@
   $nbresHeurseCoursParJours = 4;
 
   $horaireSemaines = [];
-  $VHCours = [];
-  for ($i=0; $i < count($cour); $i++) { 
-     if (!isset($VHCours[$cour[$i]['cour']])) {
-        $VHCours[$cour[$i]['cour']]['VH'] = $cour[$i]['vh'];
-     }
-  }
+ 
 
 $backUpCour = $cour;
   for ($joursemaine=0; $joursemaine < $nombresDeJour; $joursemaine++) { 
     $counterAutres = 0;
     for ($cptHeuresJours=0; $cptHeuresJours < $nombresHeuresParJours; $cptHeuresJours++) {  
-         if (isset($horaireSemaines[$joursemaine][ isset($cour[$cptHeuresJours])?$cour[$cptHeuresJours]['cour']:''])) { 
-           // faire rien
-         }else {
-           if (count($cour) < $nombresHeuresParJours && $cptHeuresJours>=count($cour) && $cour[abs($cptHeuresJours-count($cour))]['vh']) {
-                if (isset($cour[abs($cptHeuresJours-count($cour))])?$cour[abs($cptHeuresJours-count($cour))]['cour']:'') {  
-                    $cour[abs($cptHeuresJours-count($cour))]['vh'] = $cour[abs($cptHeuresJours-count($cour))]['vh']-1;  
-                    $horaireSemaines[$joursemaine][$cour[abs($cptHeuresJours-count($cour))]['cour']][$cptHeuresJours+1] = array('cour'=>$cour[abs($cptHeuresJours-count($cour))]);
+        if (count($cour) > $cptHeuresJours) {
+          # code...
+        }
+        if (isset($cour[$cptHeuresJours])?$cour[$cptHeuresJours]['vh'] > 0: false) {
+           $cour[$cptHeuresJours]['vh'] = $cour[$cptHeuresJours]['vh']-1;
+           $horaireSemaines[$joursemaine]['A'.$cour[$cptHeuresJours]['cour']] = $cour[$cptHeuresJours];
+        }else{
+          if (isset($cour[$nombresHeuresParJours+$counterAutres])?$cour[$nombresHeuresParJours+$counterAutres]['vh'] > 0: false) {
+                if (count($cour) > $nombresHeuresParJours+$counterAutres){
+                  $cour[($nombresHeuresParJours+$counterAutres)]['vh'] = $cour[$nombresHeuresParJours+$counterAutres]['vh']-1;
+                  $horaireSemaines[$joursemaine]['B'.$cour[($nombresHeuresParJours+$counterAutres)]['cour']] = $cour[$nombresHeuresParJours+$counterAutres];
+                  $counterAutres ++;
                 }
-           }  else { 
-              if ((isset($cour[$cptHeuresJours])?$cour[$cptHeuresJours]['cour']:'') && ($cour[$cptHeuresJours]['vh'])>0) { 
-                  $horaireSemaines[$joursemaine][$cour[$cptHeuresJours]['cour']][$cptHeuresJours+1] = array('cour'=>$cour[$cptHeuresJours]); 
-                  $cour[$cptHeuresJours]['vh'] = $cour[$cptHeuresJours]['vh']-1;  
-               }else {
-                    //  retourner au premier index des cours 
-                      if (abs($cptHeuresJours-(count($cour)-1)) != $counterAutres) {
-                        
-                      }else {
-                        if ($counterAutres <= count($cour)) {
-                           $counterAutres ++;
-                        } 
-                      }
-                       
-                      if (isset($cour[$counterAutres])?$cour[$counterAutres]['cour']:'') { 
-                          $horaireSemaines[$joursemaine][$cour[$counterAutres]['cour']][$cptHeuresJours+1] =
-                           array('cour'=>$cour[abs($cptHeuresJours-(count($cour)-1))]); 
-                          $cour[$counterAutres]['vh'] = $cour[$counterAutres]['vh']-1;  
-                          if ($counterAutres <= count($cour)) {
-                            $counterAutres ++;
-                         } 
-                      } 
-               }
-           }
-              
-         } 
+          }
+        }
+     }
+     if ($nombresHeuresParJours > (count($horaireSemaines[$joursemaine]))) {
+          $counterAutresDeux = 0;
+          $testFini = 0;
+          for ($indiceCour=0; 
+               $indiceCour < (count($cour));
+               $indiceCour++) {  
+         if ($testFini < ($nombresHeuresParJours - (count($horaireSemaines[$joursemaine])))) {
+         
+            if (isset($cour[$indiceCour])?$cour[$indiceCour]['vh'] > 0:false) {
+              $testFini ++; 
+              $cour[$indiceCour]['vh'] = $cour[$indiceCour]['vh']-1;
+              $horaireSemaines[$joursemaine]['C'.$cour[$indiceCour]['cour']] = $cour[$indiceCour];
+             
+            }else{
+              if (isset($cour[$indiceCour+$counterAutresDeux])?$cour[$indiceCour+$counterAutresDeux]['vh'] > 0:false) {
+                    if (count($cour) > $indiceCour+$counterAutresDeux){
+                      $cour[($indiceCour+$counterAutresDeux)]['vh'] = $cour[$indiceCour+$counterAutresDeux]['vh']-1;
+                      $horaireSemaines[$joursemaine]['D'.$cour[($indiceCour+$counterAutresDeux)]['cour']] = $cour[$indiceCour+$counterAutresDeux];
+                      $counterAutresDeux ++;
+                    }
+              }
+            }  # code...
+          }
+        } 
      }  
 }
-echo json_encode($horaireSemaines);
+// echo json_encode($horaireSemaines);
 ?>
+
+
+<!DOCTYPE>
+<html>
+     <head>
+         <title>T.Table</title>
+     </head>
+     <body>
+           <table border="1">
+                  <thead>
+                       <tr>
+                           <th>Jour</th>
+                           <th>De 7h30-8h15</th>
+                           <th>De 8h15-9h00</th>
+                           <th>De 9h00-9h45</th>
+                           <th>De 9h45-10h30</th>
+                           <th>De 10h45-11h15</th>
+                           <th>De 11h15-12h00</th>
+                           <th>De 12h00-12h45</th>
+                           <th>De 12h45-13h15</th> 
+                       </tr>
+                  </thead>
+                  <tbody>
+                       <?php 
+                          $tableData = $horaireSemaines;
+                          $arrayChanged = array_values($horaireSemaines); 
+                          for ($i=0; $i < count($arrayChanged); $i++) {  
+                       ?>
+                        <tr> 
+                          <td>
+                             Jour <?=$i+1?>
+                          </td>
+                              <?php $unite = array_values($arrayChanged[$i])  ;
+                              for ($j=0; $j < count($unite); $j++) { 
+                                ?>
+                                  <td>
+                                     <?php echo ($unite[$j]['cour']); 
+                                     ?>
+                                  </td>
+                                <?php
+                              } 
+                            ?>
+                        </tr>
+                        <?php  
+                          } 
+                          ?>
+                  </tbody>
+           </table>
+     </body>
+</html>
